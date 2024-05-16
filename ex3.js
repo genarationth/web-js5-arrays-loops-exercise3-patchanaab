1bcd4b66b6701a0909737f1a5b191a0deb9670cd
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-16 08:42:31 +0700

    exercise3

---
+++ ex3.js
@@ -0,0 +1,45 @@
+// ex3
+let star = "";
+for (j = 0; j < 5; j++) {
+	for (k = 0; k < 1; k++) {
+		star += "* ";
+		console.log(star);
+	}
+}
+
+// ex4
+let x = 4;
+while (x >= 0) {
+	console.log(`${x} Value`);
+	x -= 0.5;
+}
+
+let odd = 1;
+while (odd < 100) {
+	console.log(odd);
+	odd += 2;
+}
+
+
+let a = 1;
+let b = 6;
+while (a <= b) {
+	console.log([a]);
+	a++;
+}
+
+
+let c = 1;
+let d= 5;
+let sum = 0;
+while (c <= d) {
+	sum += c;
+	c++;
+}
+console.log(sum);
+
+
+
+
+
+
